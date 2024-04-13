5aa387eb4b09c95dca0cae3f349a5c25d1d1b032
jmuszyns <j.muszynski@stud.elka.pw.edu.pl>
2024-04-13 17:33:45 +0200

    every 3 lowercase char

--- exercises_riscv/reverse_text.asm
+++ exercises_riscv/reverse_text.asm
@@ -1,7 +1,7 @@
 		.data
 prompt: 	.asciz "Enter string:\n"
 buf:		.space 100
-rev: 		.space 100
+res: 		.space 100
 
 	.text
 	.globl main
@@ -15,28 +15,44 @@ main:
 	li a1, 100
 	ecall
 	
-	li t0, '0'
-	li t1, '9'
+	li t0, 'A'
+	li t1, 'Z'
 	
 	la t2, buf 	#adres do pierwszego znaku z bufora
-	la t3, rev 	#adres do pierwszego znaku niebêd¹cego cyfr¹
+	la t3, res	#adres do pierwszego znaku wyjœciowego bufora
 	lb t4, (t2) 	#loadujesz pierwszy znak z bufora
 	beqz t4, end
 loop:
-	addi t2, t2,1
-	lb t4, (t2)
-	bnez t4, loop
+	blt t4, t0, not_upper
+	bgt t4, t1, not_upper
 
-reverse:
-	addi t2, t2, -1
-	lb t4, (t2)
+first_upper_sq:
 	sb t4, (t3)
 	addi t3, t3, 1
-	bnez t4, reverse
+	addi t2, t2, 1
+	blt t4, t0, not_upper
+	bgt t4, t1, not_upper
+	
+upper_sq:
+	addi t2, t2, 1
+	blt t4, t0, not_upper
+	bgt t4, t1, not_upper
+	bnez t4, upper_sq
+	b end
+	
+
+not_upper:
+	sb t4, (t3)
+	addi t3, t3, 1
+	addi t2, t2, 1
+	lb t4, (t2)
+	bnez t4, loop	
+
+
 
 end:
 	li a7, 4
-	la a0, rev
+	la a0, res
 	ecall
 	
 	
