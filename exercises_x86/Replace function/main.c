32f0d87da822a524c9b4d3e22404409615a5df30
mlewko <01180803@pw.edu.pl>
2024-05-06 21:45:30 +0200

    Upload New File

---
+++ exercises_x86/Replace function/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+extern int replace(char *s);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s <string>\n", argv[0]);
+        return -1;
+    }
+
+    char *str = argv[1];
+    int str_len = strlen(str);
+
+    printf("Source: %s\n", str);
+    int result = replace(str);
+    printf("Result: %s\n", str);
+    printf("Number of small letters: %d\n", result);
+
+    return 0;
+}
