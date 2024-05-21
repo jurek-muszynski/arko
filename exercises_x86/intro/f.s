93aa364a2885e98840c0293e8881f2fc701219bf
jurek-muszynski <jurek.muszynski.poczta@gmail.com>
2024-05-21 22:19:48 +0200

    intro problem

--- exercises_x86/intro/f.s
+++ exercises_x86/intro/f.s
@@ -1,4 +1,22 @@
 section .text
-global f
-f:
+global func
+
+func:
+    push ebp
+    mov ebp, esp
+    mov eax, [ebp + 8]
+    xor ecx, ecx
+
+loop1:
+    mov cl, [eax]
+    cmp ecx, 0
+    jz end
+    add cl, 2
+    mov [eax], cl
+    inc eax
+    jmp loop1
+
+end:
+    mov esp, ebp
+    pop ebp
     ret
--- exercises_x86/intro/main.c
+++ exercises_x86/intro/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-#include "f.h"
 
-int main(int argc, char *argv[]){
-    if (2 > argc){
+void func(char *);
+
+int main(int argc, char *argv[])
+{
+    if (2 > argc)
+    {
         printf("Arg missing.\n");
         return -1;
     }
-    f(argv[1]);
+    func(argv[1]);
     printf("%s\n", argv[1]);
     return 0;
 }
