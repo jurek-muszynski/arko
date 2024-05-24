ccb798b2b92854fe838c95f88b608f534206d7f4
jurek-muszynski <jurek.muszynski.poczta@gmail.com>
2024-05-24 16:45:58 +0200

    fixes to remove nth method

--- exercises_x86/Remove nth/main.c
+++ exercises_x86/Remove nth/main.c
@@ -8,7 +8,17 @@ int main(int argc, char *argv[])
         printf("Arg missing.\n");
         return -1;
     }
-    func(argv[1], 2);
-    printf("%s\n", argv[1]);
+
+    int n;
+    sscanf(argv[2], "%d", &n);
+    printf("Initial string: %s\n", argv[1]);
+    if (!n)
+    {
+        printf("Invalid arg.\n");
+        return -1;
+    }
+    printf("Remove every %d character\n", n);
+    func(argv[1], n);
+    printf("Result: %s\n", argv[1]);
     return 0;
 }
